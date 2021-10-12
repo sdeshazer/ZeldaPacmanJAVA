528918a518560821b48ca2733493575d613ac6f4
sdeshazer <sdeshazer92@gmail.com>
2021-10-11 16:41:16 -0800

    added a typical pacman maze

---
+++ src/projectone/Node.java
@@ -0,0 +1,14 @@
+package projectone;
+
+
+public class Node {
+		public int x;
+		public int y;
+		public int cost;
+		public int previousx;
+		public int previousy;
+
+		Node parent;
+		Node succsessor;
+
+}
